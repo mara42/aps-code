132d68d1d1a4231a2fa89014fe08935afd9e803a
Martti <martti@aukia.com>
2019-02-19 19:39:54 +0000

    cleanup

--- pancake_flip/tempCodeRunnerFile.py
+++ pancake_flip/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-print
+  # try:
+    #     for line in sys.stdin:
+    #         arrays.append([int(e) for e in line.split(' ') if e])
+    # except TypeError as e:
+    #     print(e)
+    #     sys.exit()
